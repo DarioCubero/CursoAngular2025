937dd2b8672580a06d02759f6f33ccfa2f02b1ae
Dario Cubero <dariocuberozgz@gmail.com>
2025-10-01 20:54:24 +0200

    Zoneless, Readonly Signals, Pipes

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -8,12 +8,22 @@ import { Component, signal } from "@angular/core";
     margin: 5px 10px;
     width: 75px;
   }
-  `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush //Hace que no se redibuje el componente a menos que cambie una @Input
 })
 
 export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10);
+
+constructor() {
+  setInterval(() => {
+    // this.counter += 1;
+    // this.counterSignal.update( v => v + 1 );
+    this.increaseBy(1);
+    console.log('Tick');
+  }, 2000);
+}
 
   increaseBy(value: number): void {
     this.counter += value;
